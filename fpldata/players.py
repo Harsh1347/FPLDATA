ceaaf22b8f3ca8255acb303073169a68e3966cf9
Pratyush Kerhalkar <pratyushkerhalkar@gmail.com>
2020-10-09 17:35:22 +0530

    PEP:8 reformat

--- fpldata/players.py
+++ fpldata/players.py
@@ -5,42 +5,94 @@ import pandas
 class PlayerData:
     """
     Module for getting players information
-    """    
+    """
 
     def __init__(self):
         URL = f"https://fantasy.premierleague.com/api/bootstrap-static/"
         resp = requests.get(URL).json()
-        
+
         self.teams = resp['teams']
         self.events = resp['events']
         self.elements = resp['elements']
-        self.info = ['chance_of_playing_next_round', 'chance_of_playing_this_round', 'code', 'cost_change_event',
-    'cost_change_event_fall', 'cost_change_start', 'cost_change_start_fall', 'dreamteam_count',
-    'element_type', 'ep_next', 'ep_this', 'event_points', 'first_name', 'form', 'id', 'in_dreamteam',
-    'news', 'news_added', 'now_cost', 'photo', 'points_per_game', 'second_name', 'selected_by_percent', 
-    'special', 'squad_number', 'status', 'team', 'team_code', 'total_points', 'transfers_in', 
-    'transfers_in_event', 'transfers_out', 'transfers_out_event', 'value_form', 'value_season','web_name', 
-    'minutes', 'goals_scored', 'assists', 'clean_sheets', 'goals_conceded', 'own_goals', 'penalties_saved',
-    'penalties_missed', 'yellow_cards', 'red_cards', 'saves', 'bonus', 'bps', 'influence', 'creativity', 
-    'threat', 'ict_index', 'influence_rank', 'influence_rank_type', 'creativity_rank', 'creativity_rank_type',
-    'threat_rank', 'threat_rank_type', 'ict_index_rank', 'ict_index_rank_type']    
-        self.player_info = {k : [] for k in self.info}
+        self.info = [
+            'chance_of_playing_next_round',
+            'chance_of_playing_this_round',
+            'code',
+            'cost_change_event',
+            'cost_change_event_fall',
+            'cost_change_start',
+            'cost_change_start_fall',
+            'dreamteam_count',
+            'element_type',
+            'ep_next',
+            'ep_this',
+            'event_points',
+            'first_name',
+            'form',
+            'id',
+            'in_dreamteam',
+            'news',
+            'news_added',
+            'now_cost',
+            'photo',
+            'points_per_game',
+            'second_name',
+            'selected_by_percent',
+            'special',
+            'squad_number',
+            'status',
+            'team',
+            'team_code',
+            'total_points',
+            'transfers_in',
+            'transfers_in_event',
+            'transfers_out',
+            'transfers_out_event',
+            'value_form',
+            'value_season',
+            'web_name',
+            'minutes',
+            'goals_scored',
+            'assists',
+            'clean_sheets',
+            'goals_conceded',
+            'own_goals',
+            'penalties_saved',
+            'penalties_missed',
+            'yellow_cards',
+            'red_cards',
+            'saves',
+            'bonus',
+            'bps',
+            'influence',
+            'creativity',
+            'threat',
+            'ict_index',
+            'influence_rank',
+            'influence_rank_type',
+            'creativity_rank',
+            'creativity_rank_type',
+            'threat_rank',
+            'threat_rank_type',
+            'ict_index_rank',
+            'ict_index_rank_type']
+        self.player_info = {k: [] for k in self.info}
         for player in self.elements:
             for v in self.player_info:
                 self.player_info[v].append(player[v])
-        
-    
+
     def all_player_info(self):
         """
         Gives information about all the players as a pandas DataFrame
-        
+
         """
         self.data = pandas.DataFrame(self.player_info)
-        self.data['element_type'] = self.data['element_type'].replace([1,2,3,4],['GKP','DEF','MID','FWD'])
-        
+        self.data['element_type'] = self.data['element_type'].replace(
+            [1, 2, 3, 4], ['GKP', 'DEF', 'MID', 'FWD'])
+
         return self.data
 
-    def search_player(self,player_name):
+    def search_player(self, player_name):
         """
         Allow you to search Player by Name. Returns a dictionary with player id as key and name as value.
 
@@ -49,23 +101,24 @@ class PlayerData:
         """
         self.op = {}
         for player in self.elements:
-            if player_name.lower() in player['first_name'].lower() or  player_name.lower() in player['second_name'].lower():
-                self.op[player['id']] = player['first_name']+" "+player['second_name']
+            if player_name.lower() in player['first_name'].lower(
+            ) or player_name.lower() in player['second_name'].lower():
+                self.op[player['id']] = player['first_name'] + \
+                    " " + player['second_name']
         return self.op
 
     def player_list(self):
-        
         """
         Returns a dictionary of all the players with player id as key and name as value.
         """
 
         self.players = {}
         for player in self.elements:
-            self.players[player['id']] = player['first_name']+" "+player['second_name']
+            self.players[player['id']] = player['first_name'] + \
+                " " + player['second_name']
         return self.players
-    
-    def player_detail(self,player_id):
-        
+
+    def player_detail(self, player_id):
         """
         Returns a dictonary of player using the player_id
 
--- fpldata/team_data.py
+++ fpldata/team_data.py
@@ -1,12 +1,13 @@
 import requests
 import pandas
+
+
 class TeamData:
     """
     Module for getting data related to team.
     """
-    
-    
-    def __init__(self):    
+
+    def __init__(self):
         URL = f"https://fantasy.premierleague.com/api/bootstrap-static/"
         resp = requests.get(URL).json()
         self.teams = resp['teams']
@@ -18,42 +19,53 @@ class TeamData:
             self.team_name[team['short_name']] = team['name']
             self.team_id[team['short_name']] = team['id']
 
-    def team_compare(self,team1,team2):
+    def team_compare(self, team1, team2):
         """
         Returns a pandas dataframe comparing two teams based on their abilities.
 
         :param team1: Short name of the first team.
         :type team1: string
-        
+
         :param team2: Short name of the second team.
         :type team2: string
         """
 
-        self.stats = {'Team':[],'Strength':[],'Overall Home Strength':[],'Overall Away Strength':[],
-                'Home Attack':[],'Away Attack':[],'Home Defence':[],'Away Defence':[] }
-        self.team_list = [team1,team2]
+        self.stats = {
+            'Team': [],
+            'Strength': [],
+            'Overall Home Strength': [],
+            'Overall Away Strength': [],
+            'Home Attack': [],
+            'Away Attack': [],
+            'Home Defence': [],
+            'Away Defence': []}
+        self.team_list = [team1, team2]
         for team in self.team_list:
             for t in self.teams:
                 if t['short_name'].lower() == team.lower():
                     self.stats['Team'].append(t['short_name'])
-                    self.stats['Strength'].append(t['strength'] )
-                    self.stats['Overall Home Strength'].append(t['strength_overall_home'])
-                    self.stats['Overall Away Strength'].append(t['strength_overall_away'])
+                    self.stats['Strength'].append(t['strength'])
+                    self.stats['Overall Home Strength'].append(
+                        t['strength_overall_home'])
+                    self.stats['Overall Away Strength'].append(
+                        t['strength_overall_away'])
                     self.stats['Home Attack'].append(t['strength_attack_home'])
                     self.stats['Away Attack'].append(t['strength_attack_away'])
-                    self.stats['Home Defence'].append(t['strength_defence_home'])
-                    self.stats['Away Defence'].append(t['strength_defence_away'])
-        self.data = pandas.DataFrame(data=self.stats,index=self.stats['Team'])
+                    self.stats['Home Defence'].append(
+                        t['strength_defence_home'])
+                    self.stats['Away Defence'].append(
+                        t['strength_defence_away'])
+        self.data = pandas.DataFrame(data=self.stats, index=self.stats['Team'])
         del(self.data['Team'])
         return self.data
-    
+
     def list_team_name(self):
         """
         Returns a dictionary with team id as key and Short Team Name as value.
         """
         return self.team_name
 
-    def get_team_id(self,team1 = ""):
+    def get_team_id(self, team1=""):
         """
         Returns the team id if team1 parameter is provided else a dictionary is returned with all the team id and corresponding short name.
 
@@ -69,9 +81,17 @@ class TeamData:
 
     def get_table(self):
         """
-        Returns EPL table as a pandas dataframe 
+        Returns EPL table as a pandas dataframe
         """
-        self.table = {'Position':[],'Team':[],'Games Played':[],'Win':[],'Draw':[],'Loss':[],'Points':[],'Form':[]}
+        self.table = {
+            'Position': [],
+            'Team': [],
+            'Games Played': [],
+            'Win': [],
+            'Draw': [],
+            'Loss': [],
+            'Points': [],
+            'Form': []}
         for team in self.teams:
             self.table['Position'].append(team['position'])
             self.table['Team'].append(team['name'])
@@ -81,7 +101,8 @@ class TeamData:
             self.table['Loss'].append(team['loss'])
             self.table['Points'].append(team['points'])
             self.table['Form'].append(team['form'])
-        self.table = pandas.DataFrame(data=self.table,index= self.table['Position'])
-        
+        self.table = pandas.DataFrame(
+            data=self.table, index=self.table['Position'])
+
         del(self.table['Position'])
         return self.table
